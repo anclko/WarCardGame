e73babb9a1feb289ff87cf998a7c9075a22d91e4
Anne-Claire <116877661+anclko@users.noreply.github.com>
2023-02-23 16:41:31 +0100

    added deck action functions

--- Project2/src/deck.py
+++ Project2/src/deck.py
@@ -15,3 +15,18 @@ class Deck:
             self.build()
 
     # ---- GETTER ---- #
+    @property
+    def size(self):
+        return len(self._cards)
+
+    # ---- DECK ACTIONS ---- #
+    """ Building a deck, with a card of each value in its suit """
+    def build(self):
+        for suit in Deck.SUITS:
+            for value in range(2, 15):
+                self._cards.append(Card(Suit(suit), value))
+    
+    """ Show the card from deck """
+    def show(self):
+        for card in self._cards:
+            card.show()

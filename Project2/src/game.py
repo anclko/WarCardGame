45802349cef3398f64112da4e30e617b0de3832c
Anne-Claire <116877661+anclko@users.noreply.github.com>
2023-02-23 17:05:16 +0100

    updated the print_welcome_message function

--- Project2/src/game.py
+++ Project2/src/game.py
@@ -34,5 +34,43 @@ class Game:
     def print_stats(self):
         pass
 
-    def welcome_message(self):
+class Game:
+
+    PLAYER1 = 0
+    PLAYER2 = 1
+    TIE = 2
+
+    def __init__(self, player, player2, deck):
         pass
+
+    def make_initial_deck(self):
+        pass
+
+    def make_deck(self, thisPlayer):
+        pass
+
+    def battle(self, WarCards=None):
+        pass
+
+    def get_round_winner(self, player1_card, player2_card):
+        pass
+
+    def get_cards_won(self, player_card, computer_card, previous_cards):
+        pass
+
+    def add_cards_to_thisPlayer(self, thisPlayer, list_of_cards):
+        pass
+
+    def war(self, battleCards):
+        pass
+
+    def game_over(self):
+        pass
+
+    def print_stats(self):
+        pass
+
+    def print_welcome_message(self):
+        print("♣----------------------------♦")
+        print("|        War Card Game       |")
+        print("♥----------------------------♠")

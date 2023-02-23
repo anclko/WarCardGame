3483028b05b55132df942759ea10c598e8a82303
Anne-Claire <116877661+anclko@users.noreply.github.com>
2023-02-23 16:48:42 +0100

    added shuffle, draw and add methods

--- Project2/src/deck.py
+++ Project2/src/deck.py
@@ -2,6 +2,7 @@ import random
 from card import Card
 from suit import Suit
 
+
 class Deck:
     """ Build the card deck """
     SUITS = ("Club", "Diamonds", "Hearts", "Spades")
@@ -25,8 +26,25 @@ class Deck:
         for suit in Deck.SUITS:
             for value in range(2, 15):
                 self._cards.append(Card(Suit(suit), value))
-    
+
     """ Show the card from deck """
     def show(self):
         for card in self._cards:
             card.show()
+
+    """ adding random cards into the deck """
+    def shuffle(self):
+        random.shuffle(self._cards)
+
+    """ Drawing a card from deck to play """
+    def draw(self):
+        if self._cards:
+            # drawing card from top of deck, so has to be from
+            # end of the list - pop()
+            return self._cards.pop()
+        else:
+            return None
+
+    """ Add cards to the beginning of deck for cards won """
+    def add(self, card):
+        self._cards.insert(0, card)

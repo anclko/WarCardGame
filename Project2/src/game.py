3ac763114f28c07a93cd4137d9a267672afa3f39
Anne-Claire <116877661+anclko@users.noreply.github.com>
2023-02-23 20:47:11 +0100

    added game mode and initial deck creation

--- Project2/src/game.py
+++ Project2/src/game.py
@@ -12,11 +12,27 @@ class Game:
         self._make_initial_deck()
 
     def ask_game_mode(self):
+        """ Choose number of players"""
+        """1 for one player (Player vs PC), 2 for two players (PVP)"""
+
         while self._game_mode not in ["1", "2"]:
             self._game_mode = input("One (1) or Two (2) players?")
 
     def make_initial_deck(self):
-        pass
+        """ Make decks based on game mode chosen"""
+        
+        self._deck.shuffle()
+        self.ask_game_mode()
+
+        if self._game_mode == "1":
+            self._player1 = input("Enter Player 1 name: ")
+            self._player2 = input("Enter Player 2 name: ")
+        else:
+            self._player1 = input("Enter Player 1 name: ")
+            self._player2 = "Computer"
+
+        self.make_deck(self._player1)
+        self.make_deck(self._player2)
 
     def make_deck(self, thisPlayer):
         pass

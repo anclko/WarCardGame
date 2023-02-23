0f3e6b609382c282d8a5eda91bef2599f42f3e66
IbMoh <115637313+IbMoh@users.noreply.github.com>
2023-02-23 20:56:15 +0100

    Added some changes

--- Project2/src/shell.py
+++ Project2/src/shell.py
@@ -6,29 +6,32 @@ class Shell(cmd.Cmd):
     intro = "Welcome to the game. Type help or ? to list commands.\n"
     prompt = '(game) '
     
-    def __init__(self,game):
+    def __init__(self):
         super().__init__()
-        self.game = game.Game()
+        self.game = Game.Game()
     
-    def do_Start(self):
-        pass
+    def do_Start(self,_):
+        self.game.ask_game_mode()
     
-    def do_Exit(self,line):
+    def do_Exit(self,_):
         '''Exits the program'''
         print('Bye Bye!')
         return True
     
-    def do_restart(self,line):
+    def do_restart(self,_):
         '''Restarts The Game'''
-        return self.do_Start()
+        return self.do_Start(self)
     
-    def do_Cheat():
+    def do_Cheat(self,_):
         '''Win The Game By Cheating'''
         pass
 
-    def do_ChangeName():
+    def do_ChangeName(self,_):
         '''Change your Name'''
         pass
+
+    def do_Stats(self,_):
+        self.game.print_stats()
     
 if __name__ == "__main__":
     Shell().cmdloop()

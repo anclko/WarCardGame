d8e5fa869c997dd14ca921e0c65538fab5836bcb
Anne-Claire <116877661+anclko@users.noreply.github.com>
2023-02-23 16:36:27 +0100

    adde constructor and beginning of getters

--- Project2/src/deck.py
+++ Project2/src/deck.py
@@ -1,3 +1,17 @@
 import random
 from card import Card
 from suit import Suit
+
+class Deck:
+    """ Build the card deck """
+    SUITS = ("Club", "Diamonds", "Hearts", "Spades")
+
+    def __init__(self, is_empty=False):
+        # Creating empty deck of card
+        self._cards = []
+
+        # Start with empty deck, if deck empty, build deck
+        if not is_empty:
+            self.build()
+
+    # ---- GETTER ---- #

8cb5784ab5d189fcaf0b09f324d9986b97969f4c
IbMoh <115637313+IbMoh@users.noreply.github.com>
2023-02-23 20:34:38 +0100

    added intelligence class

---
+++ Project2/src/intelligence.py
@@ -0,0 +1,16 @@
+# CPU side and cheating implementation in this
+# When the computer is playing, it should have some type of 
+# intelligence for playing the game. There might be several 
+# settings för the level of intelligence for the computer playing. 
+# These should be configurable by the user while playing.
+
+
+import random
+class Intelligence:
+
+    def easy(self,deck):
+        return random.choice(deck)
+    
+    
+    def hard(self,deck):
+        return max(deck)

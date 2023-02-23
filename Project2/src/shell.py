86434b6d1be72d991f151d2311a71dcf09586518
IbMoh <115637313+IbMoh@users.noreply.github.com>
2023-02-23 20:33:34 +0100

    Updated shell NOT COMPLETE

--- Project2/src/shell.py
+++ Project2/src/shell.py
@@ -1,59 +1,34 @@
 import cmd
-import game
-
+import Game
 
 class Shell(cmd.Cmd):
-    """Example of class with command actions to roll a dice."""
 
-    intro = "Welcome to the War Game! Type help or ? to list commands.\n"
-    prompt = "(game) "
-
-    def __init__(self):
-        """Init the object."""
+    intro = "Welcome to the game. Type help or ? to list commands.\n"
+    prompt = '(game) '
+    
+    def __init__(self,game):
         super().__init__()
         self.game = game.Game()
-
-    def do_start(self, _):
-        """Start the game with a new number."""
-        msg = (
-            "I am ready and is now thinking of a new secret number"
-            " between {} and {}."
-        )
-        self.game.start()
-        print(msg.format(self.game.low(), self.game.high()))
-
-    def do_cheat(self, _):
-        """Cheat to view the secret number."""
-        print("Cheater... the number is {}.".format(self.game.cheat()))
-
-    def do_guess(self, arg):
-        """Do a guess of a number."""
-        msg = "Missing argument on the number you are guessing. Try 'guess 42"
-        if not arg:
-            print(msg)
-            return
-
-        a_number = int(arg)
-        try:
-            print("Your'e guess is -> {}".format(self.game.guess(a_number)))
-        except ValueError as error:
-            print(error)
-
-    def do_exit(self, _):
-        # pylint: disable=no-self-use
-        """Leave the game."""
-        print("Bye bye - see ya soon again")
+    
+    def do_Start(self):
+        pass
+    
+    def do_Exit(self,line):
+        '''Exits the program'''
+        print('Bye Bye!')
         return True
+    
+    def do_restart(self,line):
+        '''Restarts The Game'''
+        return self.do_Start()
+    
+    def do_Cheat():
+        '''Win The Game By Cheating'''
+        pass
 
-    def do_quit(self, arg):
-        """Leave the game."""
-        return self.do_exit(arg)
-
-    def do_quitty(self, arg):
-        """Leave the game."""
-        return self.do_exit(arg)
-
-    def do_EOF(self, arg):
-        # pylint: disable=invalid-name
-        """Leave the game."""
-        return self.do_exit(arg)
+    def do_ChangeName():
+        '''Change your Name'''
+        pass
+    
+if __name__ == "__main__":
+    Shell().cmdloop()

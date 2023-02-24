b1f1baa123639a9d3cebbaecbf59f1a7e1a19c40
Anne-Claire <116877661+anclko@users.noreply.github.com>
2023-02-24 13:55:04 +0100

    imports added

--- Project2/src/main.py
+++ Project2/src/main.py
@@ -1,27 +1,8 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+from deck import Deck
+from player import Player
+from war_card_game import WarCardGame
 
 """
-Lets play a game of "Guess my number".
-
-I will be thinking af a number between 1 and 100.
-You shall try to guess the number.
-
-I will let you know if your guess is lower or higher than
-the number that I am thinking of.
-
-You have 5 guesses. Then you loose.
-
+RULES HERE
 """
 
-import shell
-
-
-def main():
-    """Execute the main program."""
-    print(__doc__)
-    shell.Shell().cmdloop()
-
-
-if __name__ == "__main__":
-    main()

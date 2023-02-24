df070bf0bfc8afa6137cb5d3a6b422aacfe24147
Anne-Claire <116877661+anclko@users.noreply.github.com>
2023-02-24 13:19:31 +0100

    Made changes to revert to basic game

--- Project2/src/game.py
+++ Project2/src/game.py
@@ -1,37 +1,21 @@
-class Game:
+class WarCardGame:
 
-    PLAYER1 = 0
+    PLAYER = 0
     PLAYER2 = 1
     TIE = 2
 
-    def __init__(self, player1, player2, deck):
+    def __init__(self, player, player2, deck):
+        self._player = player
+        self._player2 = player2
         self._deck = deck
-        self._player1 = None
-        self._player2 = None
-        self._game_mode = None
-        self._make_initial_deck()
-
-    def ask_game_mode(self):
-        """ Choose number of players"""
-        """1 for one player (Player vs PC), 2 for two players (PVP)"""
-
-        while self._game_mode not in ["1", "2"]:
-            self._game_mode = input("One (1) or Two (2) players?")
-
+        self.make_initial_decks()
+    
     def make_initial_deck(self):
         """ Make decks based on game mode chosen"""
+        """ Code right now only works with player and PC mode"""
         
         self._deck.shuffle()
-        self.ask_game_mode()
-
-        if self._game_mode == "1":
-            self._player1 = input("Enter Player 1 name: ")
-            self._player2 = input("Enter Player 2 name: ")
-        else:
-            self._player1 = input("Enter Player 1 name: ")
-            self._player2 = "Computer"
-
-        self.make_deck(self._player1)
+        self.make_deck(self._player)
         self.make_deck(self._player2)
 
     def make_deck(self, thisPlayer):

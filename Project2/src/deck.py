57187627142730a629253816c7056ea817638dd5
Anne-Claire <116877661+anclko@users.noreply.github.com>
2023-02-24 15:09:05 +0100

    Update deck.py

--- Project2/src/deck.py
+++ Project2/src/deck.py
@@ -4,47 +4,36 @@ from suit import Suit
 
 
 class Deck:
-    """ Build the card deck """
-    SUITS = ("Club", "Diamonds", "Hearts", "Spades")
+
+    SUITS = ("clubs", "diamonds", "hearts", "spades")
 
     def __init__(self, is_empty=False):
-        # Creating empty deck of card
         self._cards = []
 
-        # Start with empty deck, if deck empty, build deck
         if not is_empty:
             self.build()
 
-    # ---- GETTER ---- #
     @property
     def size(self):
         return len(self._cards)
 
-    # ---- DECK ACTIONS ---- #
-    """ Building a deck, with a card of each value in its suit """
     def build(self):
         for suit in Deck.SUITS:
             for value in range(2, 15):
                 self._cards.append(Card(Suit(suit), value))
 
-    """ Show the card from deck """
     def show(self):
         for card in self._cards:
             card.show()
 
-    """ adding random cards into the deck """
     def shuffle(self):
         random.shuffle(self._cards)
 
-    """ Drawing a card from deck to play """
     def draw(self):
         if self._cards:
-            # drawing card from top of deck, so has to be from
-            # end of the list - pop()
             return self._cards.pop()
         else:
             return None
 
-    """ Add cards to the beginning of deck for cards won """
     def add(self, card):
         self._cards.insert(0, card)

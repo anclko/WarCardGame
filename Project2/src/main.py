1670a8426cc702836061f536a157c41a56edb10d
Anne-Claire <116877661+anclko@users.noreply.github.com>
2023-02-24 17:09:14 +0100

    added game mode and updated object creation

--- Project2/src/main.py
+++ Project2/src/main.py
@@ -1,12 +1,12 @@
 from deck import Deck
 from game_player import Player
-from war_card_game import WarCardGame
+from game import WarCardGame
 
-player = Player(input("What is your name?"), Deck(is_empty=True))
-mode = input("Enter game mode, PC(1) or other player (2)")
+player = Player(input("Enter Player 1's name: "), Deck(is_empty=True))
+mode = input("Enter game mode, PC(1) or other player (2): ")
 
 if mode == "2":
-    player2 = Player(input("Enter name: "), Deck(is_empty=True))
+    player2 = Player(input("Enter Player 2's name: "), Deck(is_empty=True))
 else:
     player2 = Player("Computer", Deck(is_empty=True))
 

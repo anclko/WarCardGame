cdadaca4e7800fb5d7704b2c996eb32520f2d559
Anne-Claire <116877661+anclko@users.noreply.github.com>
2023-02-24 13:59:40 +0100

    created game object,  and choice to quit game

--- Project2/src/main.py
+++ Project2/src/main.py
@@ -9,3 +9,16 @@ RULES HERE
 player = Player("AC", Deck(is_empty=True))
 player2 = Player("Player 2", Deck(is_empty=True), is_computer=True)
 deck = Deck()
+warGame = WarCardGame(player, player2, deck)
+
+warGame.print_welcome_message()
+
+while not warGame.check_game_over():
+    warGame.start_battle()
+    warGame.print_stats()
+
+    choice = input("\nReady?\nPress Enter to continue. Enter X to stop.")
+
+    if choice.lower() == "x":
+        break
+

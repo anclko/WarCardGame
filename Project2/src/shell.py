b047fe690f0928e865e98de2076dd93c3e87bace
Anne-Claire <116877661+anclko@users.noreply.github.com>
2023-02-28 10:15:30 +0100

    fixed spacing, line too long and indentation

--- Project2/src/shell.py
+++ Project2/src/shell.py
@@ -1,35 +1,33 @@
 import cmd
-from Game import WarCardGame
+from game import WarCardGame
 from deck import Deck
-from Player import Player
-from Intelligence import Intelligence
+from player import Player
+from intelligence import Intelligence
+
 
 class Shell(cmd.Cmd):
 
     i = Intelligence()
 
-
     intro = "Type help or ? to list commands.\n"
     prompt = '> '
-    
-    
-    def do_Start(self,_):
+
+    def do_Start(self, _):
         '''Start The Game'''
 
         player1 = Player(input("Enter your name: "), Deck(is_empty=True))
         mode = input("Enter game mode 1 for PC, for 2nd Player: ")
         if mode == '2':
-            player2 = Player(input("Enter opponent name: "), Deck(is_empty=True))
+            player2 = Player(input("Player 2's name: "), Deck(is_empty=True))
         else:
             player2 = Player("Computer", Deck(is_empty=True))
-                
 
         # Create a new deck and game instance
         deck = Deck()
         game = WarCardGame(player1, player2, deck)
 
         if mode == '1':
-            defficulty = input('what level do you want the PC to be easy/hard? ')
+            defficulty = input('Select difficulty: Easy/Hard? ')
             if defficulty.lower() == 'easy':
                 self.i.easy(player2._deck)
             else:
@@ -48,16 +46,15 @@ class Shell(cmd.Cmd):
             elif asnswer == 'restart':
                 return self.do_restart(self)
 
-    def do_Exit(self,_):
+    def do_Exit(self, _):
         '''Exits the program'''
         print('Bye Bye!')
         return True
-    
-    def do_restart(self,_):
+
+    def do_restart(self, _):
         '''Restarts The Game'''
         return self.do_Start(self)
-    
-    def do_Cheat(self,_):
+
+    def do_Cheat(self, _):
         '''Win The Game By Cheating'''
         pass
-

72ab8912d0411d257b67abc857cb1e0a6e2c0b83
Anne-Claire <116877661+anclko@users.noreply.github.com>
2023-02-28 10:08:20 +0100

    modified indenting and space errors

--- Project2/src/intelligence.py
+++ Project2/src/intelligence.py
@@ -1,18 +1,16 @@
+import random
+
 # CPU side and cheating implementation in this
-# When the computer is playing, it should have some type of 
-# intelligence for playing the game. There might be several 
-# settings för the level of intelligence for the computer playing. 
+# When the computer is playing, it should have some type of
+# intelligence for playing the game. There might be several
+# settings för the level of intelligence for the computer playing.
 # These should be configurable by the user while playing.
 
 
-import random
-
 class Intelligence:
 
-    def easy(self,deck):
+    def easy(self, deck):
         return random.choice(deck._cards)
-    
-    
-    def hard(self,deck):
+
+    def hard(self, deck):
         return max(deck._cards)
-    

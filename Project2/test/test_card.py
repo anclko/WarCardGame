0c72c881307147c4e750dd115f7fd888e86c6827
IbMoh <115637313+IbMoh@users.noreply.github.com>
2023-03-06 11:02:57 +0100

    Fixed the errors

--- Project2/test/test_card.py
+++ Project2/test/test_card.py
@@ -1,38 +1,38 @@
 import unittest
-from src.game import WarCardGame
-from src.card import Card
-from src.suit import Suit
+from game import WarCardGame
+from card import Card
+from suit import Suit
 
 
 class TestCard(unittest.TestCase):
 
     def test_value(self):
         """Create a Card object and assert it's value"""
-        card = Card(Suit.HEARTS, 7)
+        card = Card(Suit.SYMBOLS['hearts'], 7)
         self.assertEqual(card.value, 7)
 
     def test_suit(self):
         """Create a Card object and assert it's suit"""
-        card = Card(Suit.DIAMONDS, 10)
-        self.assertEqual(card.suit, Suit.DIAMONDS)
+        card = Card(Suit.SYMBOLS['diamonds'], 10)
+        self.assertEqual(card.suit, Suit.SYMBOLS['diamonds'])
 
     def test_special(self):
         """ Create a special card and assert it's 'specialness'"""
-        card1 = Card(Suit.SPADES, 10)
-        card2 = Card(Suit.CLUBS, 14)
+        card1 = Card(Suit.SYMBOLS['spades'], 10)
+        card2 = Card(Suit.SYMBOLS['clubs'], 14)
         self.assertFalse(card1.is_special())
         self.assertTrue(card2.is_special())
 
     def test_lower(self):
         """ Create Card objects and compare high value"""
-        card1 = Card(Suit.HEARTS, 7)
-        card2 = Card(Suit.SPADES, 10)
+        card1 = Card(Suit.SYMBOLS['hearts'], 7)
+        card2 = Card(Suit.SYMBOLS['spades'], 10)
         self.assertTrue(card1 < card2)
 
     def test_greater(self):
         """ Create Card objects and compare lower value"""
-        card1 = Card(Suit.DIAMONDS, 12)
-        card2 = Card(Suit.CLUBS, 5)
+        card1 = Card(Suit.SYMBOLS['diamonds'], 12)
+        card2 = Card(Suit.SYMBOLS['clubs'], 5)
         self.assertTrue(card1 > card2)
 
 

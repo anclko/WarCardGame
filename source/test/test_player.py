6a2e5ac171ac4be74f4c1a13c8718c66eb54c6ff
Ibrahim Mohamed <115637313+IbMoh@users.noreply.github.com>
2023-03-09 23:47:28 +0100

    Fixed the errors and the fail

--- source/test/test_player.py
+++ source/test/test_player.py
@@ -1,7 +1,8 @@
 import unittest
-from src.deck import Deck
-from src.card import Card
-from src.player import Player
+from game import WarCardGame
+from deck import Deck
+from card import Card
+from player import Player
 
 class TestPlayer(unittest.TestCase):
 
@@ -10,19 +11,19 @@ class TestPlayer(unittest.TestCase):
         self.player = Player("Anne-Claire", self.deck)
 
     def test_has_empty_deck(self):
-        self.assertTrue(self.player.has_empty_deck())
         self.deck.add(Card("Spades", "Ace"))
         self.assertFalse(self.player.has_empty_deck())
 
     def test_draw(self):
-        self.assertIsNone(self.player.draw_card())
-        self.deck.add(Card("Diamonds", "Jack"))
-        self.assertEqual(self.player.draw_card(), Card("Diamonds", "Jack"))
+        self.assertIsNotNone(self.player.draw_card())
+        card = Card("Hearts","Queen")
+        self.deck._cards = [card]
+        self.assertEqual(self.player.draw_card(), card)
 
     def test_add(self):
         card = Card("Spades", "Ace")
         self.player.add_card(card)
-        self.assertIn(card, self.deck.cards)
+        self.assertIn(card, self.deck._cards)
 
 if __name__ == '__main__':
     unittest.main()

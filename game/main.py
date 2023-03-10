44fa84acd6256e96b73c101e0d37bea63c6b9540
Anne-Claire <116877661+anclko@users.noreply.github.com>
2023-03-10 16:49:48 +0100

    added docstrings

--- game/main.py
+++ game/main.py
@@ -1,10 +1,18 @@
-"""Doc."""
+"""Class Main."""
 from game.shell import Shell
 
 
 def main():
-    """Doc."""
-    """also enables printing out doc strings."""
+    """Run the game by starting the command-line interface.
+
+    This function starts the game by printing a brief description of the game,
+    and then launching a command-line interface that allows the player to
+    interact with the game. The game continues until the player chooses to quit
+    the game or until the game ends.
+
+    Returns:
+    None
+    """
     print(__doc__)
     Shell().cmdloop()
 

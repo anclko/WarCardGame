8815967f859770fec71d6d16541938379730f075
Anne-Claire <116877661+anclko@users.noreply.github.com>
2023-03-10 16:43:14 +0100

    added docstrings

--- tests/test_player.py
+++ tests/test_player.py
@@ -1,30 +1,42 @@
+"""Testing player."""
 import unittest
-from game import WarCardGame
-from deck import Deck
-from card import Card
-from player import Player
+from game.deck import Deck
+from game.card import Card
+from game.player import Player
 
 
 class TestPlayer(unittest.TestCase):
+    """Test case for the Player class."""
+
     def setUp(self):
+        """Set up the test fixture."""
         self.deck = Deck()
         self.player = Player("Anne-Claire", self.deck)
 
     def test_has_empty_deck(self):
+        """Test that the has_empty_deck method.
+
+        Returns False when the player has cards in their deck.
+        """
         self.deck.add(Card("Spades", "Ace"))
         self.assertFalse(self.player.has_empty_deck())
 
     def test_draw(self):
+        """Test draw_card method so it returns a card.
+
+        and removes it from the player's deck.
+        """
         self.assertIsNotNone(self.player.draw_card())
         card = Card("Hearts", "Queen")
         self.deck._cards = [card]
         self.assertEqual(self.player.draw_card(), card)
 
     def test_add(self):
+        """Test that the add_card method adds a card to the player's deck."""
         card = Card("Spades", "Ace")
         self.player.add_card(card)
         self.assertIn(card, self.deck._cards)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()

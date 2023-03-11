7dc57688ea4ab86c120c42704bfdd94086e4bde3
Anne-Claire <116877661+anclko@users.noreply.github.com>
2023-03-11 16:11:46 +0100

    fixed pylint errors

--- tests/test_card.py
+++ tests/test_card.py
@@ -1,15 +1,20 @@
 """
 This module contains unit tests for the Card class.
 
-The Card class represents a playing card in a deck of cards. It has a suit and a value. These unit tests verify the correctness of the Card class and its methods.
+The Card class represents a playing card in a deck of cards.
+It has a suit and a value. These unit tests verify the
+correctness of the Card class and its methods.
 
 The tests in this module include:
 
 . test_value: tests that a Card object's value is set correctly
 . test_suit: tests that a Card object's suit is set correctly
-. test_special: tests that a special card is marked as special and a non-special card is not
-. test_lower: tests that a card with a lower value is less than a card with a higher value
-. test_greater: tests that a card with a higher value is greater than a card with a lower value
+. test_special: tests that a special card is marked as special
+and a non-special card is not
+. test_lower: tests that a card with a lower value is less than
+a card with a higher value
+. test_greater: tests that a card with a higher value is greater
+than a card with a lower value
 
 """
 
@@ -20,37 +25,33 @@ from game.suit import Suit
 
 
 class TestCard(unittest.TestCase):
-    """
-    
-    Unit tests for the Card class.
-    
-    """
+    """Unit tests for the Card class."""
 
     def test_value(self):
-        """Create a Card object and assert it's value"""
+        """Create a Card object and assert it's value."""
         card = Card(Suit.SYMBOLS["hearts"], 7)
         self.assertEqual(card.value, 7)
 
     def test_suit(self):
-        """Create a Card object and assert it's suit"""
+        """Create a Card object and assert it's suit."""
         card = Card(Suit.SYMBOLS["diamonds"], 10)
         self.assertEqual(card.suit, Suit.SYMBOLS["diamonds"])
 
     def test_special(self):
-        """Create a special card and assert it's 'specialness'"""
+        """Create a special card and assert it's specialness."""
         card1 = Card(Suit.SYMBOLS["spades"], 10)
         card2 = Card(Suit.SYMBOLS["clubs"], 14)
         self.assertFalse(card1.is_special())
         self.assertTrue(card2.is_special())
 
     def test_lower(self):
-        """Create Card objects and compare high value"""
+        """Create Card objects and compare high value."""
         card1 = Card(Suit.SYMBOLS["hearts"], 7)
         card2 = Card(Suit.SYMBOLS["spades"], 10)
         self.assertTrue(card1 < card2)
 
     def test_greater(self):
-        """Create Card objects and compare lower value"""
+        """Create Card objects and compare lower value."""
         card1 = Card(Suit.SYMBOLS["diamonds"], 12)
         card2 = Card(Suit.SYMBOLS["clubs"], 5)
         self.assertTrue(card1 > card2)
